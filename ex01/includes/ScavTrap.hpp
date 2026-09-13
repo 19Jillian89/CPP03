dfd01bbdda9751acb3a7ee0b4ef1fecca2ce6274
Ilaria Nassi <110412503+19Jillian89@users.noreply.github.com>
2026-09-13 22:40:37 +0200

    Add files via upload

---
+++ ex01/includes/ScavTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilnassi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/13 21:49:10 by ilnassi           #+#    #+#             */
+/*   Updated: 2026/09/13 21:55:28 by ilnassi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap 
+{
+	public:
+		ScavTrap();
+		ScavTrap(const std::string& name);
+		ScavTrap(const ScavTrap& other);
+		ScavTrap& operator=(const ScavTrap& other);
+		~ScavTrap();
+
+		//Override
+		void attack(const std::string& target);
+
+		//special ability (Subject)
+		void guardGate();
+
+};
+
+#endif
