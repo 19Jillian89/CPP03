694ff6af8d2157c6b46743281eaf5dc7219db5c9
Ilaria Nassi <110412503+19Jillian89@users.noreply.github.com>
2026-09-22 11:02:58 +0200

    Create FragTrap.hpp

---
+++ ex02/includes/FragTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilnassi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/22 10:35:39 by ilnassi           #+#    #+#             */
+/*   Updated: 2026/09/22 10:39:11 by ilnassi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap 
+{
+	public:
+		FragTrap();
+		FragTrap(const std::string& name);
+		FragTrap(const FragTrap& other);
+		FragTrap& operator=(const FragTrap& other);
+		~FragTrap();
+
+		void	highFivesGuys(void);
+};
+
+#endif
