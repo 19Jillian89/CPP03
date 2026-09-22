a65d50ba31b9a36690977c224d2356952964a4d0
Ilaria Nassi <110412503+19Jillian89@users.noreply.github.com>
2026-09-22 14:26:27 +0200

    Add files via upload

---
+++ ex03/includes/DiamondTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilnassi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/22 13:55:00 by ilnassi           #+#    #+#             */
+/*   Updated: 2026/09/22 13:58:16 by ilnassi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+class DiamondTrap : public ScavTrap, public FragTrap
+{
+	private:
+		std::string _name; //Variabile ombra
+	
+	public:
+		DiamondTrap();
+		DiamondTrap(const std::string& name);
+		DiamondTrap(const DiamondTrap& other);
+		DiamondTrap& operator=(const DiamondTrap& other);
+		~DiamondTrap();
+
+		using ScavTrap::attack;
+
+		void whoAmI();
+};
+
+#endif
